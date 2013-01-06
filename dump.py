b73df1d0ee54318aee86c33fdb301f62fa92bd01
Urs Holzer <urs@andonyar.com>
2013-01-06 14:41:53 +0100

    dump utility
    dumps the database of the project.

---
+++ dump.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import SemPipe
+from SemPipe import Project
+
+print("Building {}".format(SemPipe.path2fileurl(".", directory=True)))
+p = Project(SemPipe.path2fileurl(".", directory=True), "./sempipe_store")
+resources = p.resources
+
+print(p.g.serialize(format='n3').decode("UTF-8"))
+
+p.close()
+
